19ab22669857e48be4774b7a134ac35a1fe709f3
Gita RWS <99814080+G1taaRamadhan1@users.noreply.github.com>
2025-05-13 22:02:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,6 @@ except LookupError:
     download_nltk_resources('corpora/stopwords')
 stop_words = set(stopwords.words('english'))
 
-download_nltk_resources('corpora/wordnet')
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-
 # Fungsi untuk pra-pemrosesan teks (sesuaikan dengan yang Anda gunakan)
 def preprocess_text(text):
     if isinstance(text, str):
@@ -83,5 +79,4 @@ if st.button("Deteksi"):
 
 # Catatan kaki (opsional)
 st.markdown("---")
-st.markdown("Developed using Streamlit and a trained Logistic Regression model.")
 st.markdown("Developed using Streamlit and a trained Logistic Regression model.")
